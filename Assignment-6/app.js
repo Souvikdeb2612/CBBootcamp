55a9314aef2f041d1b200ea78b91ea180f486bd4
Souvikdeb2612 <souvikdeb26@gmail.com>
2021-04-02 13:42:16 +0530

    added icons and css

--- Assignment-6/app.js
+++ Assignment-6/app.js
@@ -1,16 +1,36 @@
 const todo =document.getElementById('inp');
 const btn = document.getElementById('addTodo');
-const list =document.getElementById('list');
+const list =document.getElementById('todo-list');
 
 btn.onclick = (e)=>{
     const todoText=todo.value;
-    const li =document.createElement('li');
-    const rem = document.createElement('button');
-    rem.innerHTML="Delete"
+    // todo Div
+    const todoDiv =document.createElement('div');
+    todoDiv.classList.add('todo');
+    // todo Li
+    const newTodo = document.createElement('li');
+    newTodo.innerText=todoText;
+    newTodo.classList.add('new-todo');
 
-    li.innerText=todoText;
-    list.append(li);
-    list.after(rem);
+    todoDiv.appendChild(newTodo);
+
+    // add edit button
+    const edit = document.createElement('button');
+    edit.innerHTML='<i class="far fa-edit"></i>';
+    edit.classList.add('edit');
+    todoDiv.appendChild(edit);
+    // add delete button
+    const trash = document.createElement('button');
+    trash.innerHTML='<i class="fas fa-trash-alt"></i>';
+    trash.classList.add('trash');
+    todoDiv.appendChild(trash);
+
+     
+
+    // add items in the list
+    list.appendChild(todoDiv);
+
+
 
     todo.value=" ";
 }
