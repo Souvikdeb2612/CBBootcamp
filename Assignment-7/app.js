861e4a8ec9abebfdc10840c801e8d0da4f401362
Souvikdeb2612 <souvikdeb26@gmail.com>
2021-04-04 13:17:14 +0530

    fetched weather data

--- Assignment-7/app.js
+++ Assignment-7/app.js
@@ -1,1 +1,37 @@
-const form = document.getElementById
+const searchForm = document.getElementById('searchForm');
+
+
+
+
+
+
+const getData = async(searchText)=>{
+    const url =`https://api.openweathermap.org/data/2.5/weather?q=${searchText}&appid=2629cb07f801fedceda63abc5f4d1c51`
+
+    await fetch(url)
+        .then(weatherData=>{
+            console.log(weatherData);
+            return weatherData.json();
+        })
+        .then(parsedData=>{
+            console.log(parsedData);
+            const cityName=console.log(parsedData.name);
+            const currTemp=console.log(parsedData.main.temp-273.15);
+            const minTemp=console.log(parsedData.main.temp_min-273.15);
+            const maxTemp=console.log(parsedData.main.temp_max-273.15);
+            const visibility=console.log(parsedData.visibility/1000);
+        })
+        .catch(err=>{
+            console.log('Error');
+            console.log(err);
+        })
+}
+
+
+
+searchForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const searchText=searchForm.elements[0].value;
+
+    getData(searchText)
+} )
